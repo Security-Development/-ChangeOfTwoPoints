dd55e171071a7550f1d3c17d329bf05b579ee0eb
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-08-19 12:29:02 +0900

    Update source.c

--- source.c
+++ source.c
@@ -4,7 +4,7 @@
 
 int main() {
 	int map[MAX][MAX] = {0,};
-	double start[2] = {2, 1}, end[2] = {29, 7}, inclination = (start[1] - end[1]) / (start[0] - end[0]); // 기울기
+	double start[2] = {2, 1}, end[2] = {29, 7};
 	
 	for(int i = 0, x = start[0], y = start[1]; !(x == end[0] && y == end[1]); i++, x = start[0] + (end[0] - start[0]) * (0.01 * i), y = start[1] + (end[1] - start[1]) * (0.01 * i)) {
 		map[y][x] = 1; // 원점 평행이동 좌표 찍기 
